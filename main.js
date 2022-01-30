1155896b1f48ab4a29d94baa54150d2f942a4607
RTBlanco <ronnytoribio1@hotmail.com>
2022-01-30 12:01:21 -0500

    added google storage

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-// import {state} from './popup.js'
+// Read it using the storage API
+chrome.storage.sync.get('on', function(items) {
+  console.log('Settings retrieved', items);
 
-document.addEventListener('DOMContentLoaded', ()=> {
+
   const frames = document.getElementsByTagName('div')
+  const state = {
+    on: items.on
+  }
+
   if (state.on) {
     for (let i = 0; i < frames.length; i ++) {
-      frames[i].className += 'turbo-frame-finder'
+      frames[i].classList.add('turbo-frame-finder')
     }
   } else {
     for (let i = 0; i < frames.length; i ++) {
       frames[i].classList.remove('turbo-frame-finder')
     }
   }
-})
+});
--- popup.js
+++ popup.js
@@ -1,13 +1,39 @@
-const state = {
-  on: false
-};
 
 document.addEventListener('DOMContentLoaded', () => {
-  localStorage.setItem('on', false)
-
+  
+  const state = {
+    on: checkStorage()
+  }
+  console.log(state.on)
   const toggleSwitch = document.getElementById('toggle-switch');
-  toggleSwitch.addEventListener('click', () => {
-    // state.on = !state.on;
+
+  toggleSwitch.checked = state.on
+
+  
+  toggleSwitch.addEventListener('change', () => {
+    state.on = !state.on;
+    // localStorage.setItem('on', state.on)
+
+    chrome.storage.sync.set({'on': state.on}, ()=> {
+      console.log('Settings saved');
+    });
   });
 });
 
+function checkStorage() {
+  // let storage = localStorage.getItem('on')
+
+  // if (typeof storage === 'undefined' || storage === 'false' ) {
+  //   return false
+  // } else {
+  //   return true
+  // }
+
+  chrome.storage.sync.get('on', function(items) {
+    console.log('Settings retrieved', items);
+    if (typeof items.on === 'undefined') {
+      return false
+    }  
+    return items.on
+  });
+}
