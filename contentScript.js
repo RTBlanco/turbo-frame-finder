a91005d3625f8604ba4fd0827603426583d35822
Ronny Toribio Blanco <ronnytoribio1@hotmail.com>
2022-01-31 14:07:43 -0500

    removed comments

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,7 @@
 // Read it using the storage API
 chrome.storage.sync.get('on', function(items) {
-  console.log('Settings retrieved', items);
-  
   const state = {on: items.on}
 
-  
   const frames = document.getElementsByTagName('turbo-frame')
   if (state.on) {
     for (let i = 0; i < frames.length; i ++) {
--- popup.js
+++ popup.js
@@ -13,16 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
   toggleSwitch.addEventListener('change', () => {
     state.on = !state.on;
-    chrome.storage.sync.set({'on': state.on}, ()=> {
-      console.log('Settings saved');
-    });
+    chrome.storage.sync.set({'on': state.on}, ()=> {});
   });
 });
 
 function checkStorage() {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get('on', function(items) {
-      console.log('Settings retrieved', items);
       resolve(items.on)
     });
   });
